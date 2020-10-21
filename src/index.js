9740463a6ce0a27c5171f7a6b819caa2f4295a7a
Vitali Pilipchuk <cpaaaik@gmail.com>
2020-10-21 22:43:09 +0300

    init: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let resultString = null;
+    while (str) {
+        for (let elem of bracketsConfig) {
+            str = str.replace(elem.join(''), '')
+        }
+        if (str === resultString) return false
+        resultString = str
+    }
+    return str.length === 0
 }
+
